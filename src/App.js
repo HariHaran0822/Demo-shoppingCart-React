2cef83932fa44928540e4c14aae44b064a7a661b
HariHaran0822 <132766301+HariHaran0822@users.noreply.github.com>
2023-05-08 18:46:53 +0530

    App.js update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+ import Navbar from './components/navbar';
+import React, { useState,useEffect} from 'react'
+import Amazon from './components/amazon';
+import Cart from './components/cart';
+import Dropdown from './components/dropDown';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+ const App=()=>{
+   const [mshow,setMshow]=useState(false);
+   const [show,setShow] = useState(true);
+   const[cart,setCart] = useState([]);
+   const[button,setButton]=useState("AddToCart")
+   const[price,setPrice]=useState(0);
+
+   
+
+   const handleClick=(item)=>{
+  if(cart.indexOf(item) !== -1) return;
+  setCart([...cart,item]);
+    }
+
+const handleRemove= (id) =>{
+  const arr = cart.filter((item) => item.id!== id );
+  setCart(arr);
+  handlePrice();
 }
+
+const handlePrice = () =>{
+  let count= 0;
+  cart.map((item)=>(count += item.amount * item.cost));
+  setPrice(count);
+  };
+
+  useEffect(()=>{
+    handlePrice();
+});
+
+const handleChange = (item,d)=>{
+  const ind = cart.indexOf(item);
+  const arr = cart;
+  arr[ind].amount += d ;
+  if(arr[ind].amount === 0){arr[ind].amount= 1 ;}
+  setCart([...arr]);
+  console.log(arr[ind].amount);
+};
+
+
+
+
+
+   return(
+<React.Fragment>
+
+  <Navbar setShow={setShow} size={cart.length}  setMshow={setMshow}  cart={cart} price={price} mshow={mshow}/>
+  <Dropdown/>
+ 
+
+  
+ { 
+ show ?  (<Amazon handleClick={handleClick}  button={button}  setButton={setButton}/>)
+ :
+ (<Cart cart={cart} 
+  setCart={setCart} 
+   handleChange={handleChange} 
+   handleRemove={handleRemove}
+  handlePrice={handlePrice}
+  setMshow={setMshow}
+  price={price}
+  mshow={mshow}
+  size={cart.length}
+ />
+  ) }
+
+
+</React.Fragment>
+   );
+ }; 
 
 export default App;
